1ef8f08d5234af335911dfaabb221867569d5937
Patricia Hidalgo-Gonzalez <paty.hidalgo.g@gmail.com>
2017-09-18 12:22:56 -0700

    New load imports from mysql (loads generated from Cui+Jimmy's script, using Max's and Shuba's updated loads for CCC3).

--- importing_tables_from_csv_ampl_to_postgresql.sql
+++ importing_tables_from_csv_ampl_to_postgresql.sql
@@ -1009,5 +1009,35 @@ values (5,
 		); 
 
 
+-- new synthetic hydro scenario	 full timeframe	
+insert into scenario
+values (6, 
+		'70% of hydro, full timepoints', 
+		'Reduced hydro to 70% from basecase, timepoints from AMPL 1112, load_id=21, 2017 fuel costs from EIA, 2016 dollars',
+		3, -- study_timeframe_id
+		3, -- time_sample_id
+		21, -- demand_scenario_id
+		2, -- fuel_simple_price_scenario
+		1, -- generation_plant_scenario_id
+		1, -- generation_plant_cost_scenario_id
+		1, -- generation_plant_existing_and_planned_scenario_id
+		4, -- hydro_simple_scenario_id
+		35 -- carbon_cap_scenario_id
+		); 
 
 
+-- new synthetic hydro scenario	 full timeframe	
+insert into scenario
+values (7, 
+		'60% of hydro, full timepoints', 
+		'Reduced hydro to 60% from basecase, timepoints from AMPL 1112, load_id=21, 2017 fuel costs from EIA, 2016 dollars',
+		3, -- study_timeframe_id
+		3, -- time_sample_id
+		21, -- demand_scenario_id
+		2, -- fuel_simple_price_scenario
+		1, -- generation_plant_scenario_id
+		1, -- generation_plant_cost_scenario_id
+		1, -- generation_plant_existing_and_planned_scenario_id
+		5, -- hydro_simple_scenario_id
+		35 -- carbon_cap_scenario_id
+		); 
--- tables_from_mysql/load_transfer/move_load_data.sh
+++ tables_from_mysql/load_transfer/move_load_data.sh
@@ -2,13 +2,12 @@
 # you'll need to edit the descriptions of the load scenarios manually.
 # You might also want to execute this script step-by-step if you want to 
 # inspect the intermediate files or keep them around before deleting them.
-mysql_db="loads_wecc_ccc3"
-load_scenario_ids="11,15"
+mysql_db="loads_wecc_ccc3_revised"
+load_scenario_ids="111,112,113,114,115,116,117"
 load_scenario_sql="
 select *,
 	load_scenario_id as demand_scenario_id, 
-	scenario_name as name,
-	description
+	scenario_name as name
 from CCC3_load_scenario_table
  	join ccc3_load_scenario_mapping USING (CCC3_load_scenario)
 where CCC3_load_scenario in ($load_scenario_ids);
@@ -22,16 +21,17 @@ select
 	timepoint_id as raw_timepoint_id, 
 	load_area as load_zone_name,
 	datetime_utc as timestamp_utc,
--- We are only drawing from one historic year for now, so skip the historic timepoint export
---	future_2006_datetime_map.datetime_utc_2006 as historic_timestamp_utc, 
 	load_MWh as demand_mw
-from CCC3_WECC_loads_aggregated_V2
+from CCC3_WECC_loads_aggregated_V3
  	join ccc3_load_scenario_mapping USING (CCC3_load_scenario)
 	join switch_inputs_wecc_v2_2.load_area_info_v3 USING (area_id)
 	join switch_inputs_wecc_v2_2.study_timepoints using(timepoint_id)
---	join future_2006_datetime_map using (timepoint_id)
 where CCC3_load_scenario in ($load_scenario_ids);
 "
+# We are only drawing from one historic year for now, so skip the historic timepoint export
+# future_2006_datetime_map.datetime_utc_2006 as historic_timestamp_utc, 
+
+
 mysql -p $mysql_db -e "$load_data_sql" > load_data.txt
 
 
